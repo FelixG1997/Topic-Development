0a1bcb0eff0a5a12d6fb2b96ed18e0a19c53ce95
Felix Greuling <Felix.Greuling@googlemail.com>
2018-09-15 13:17:51 +0200

    adding first lvl

---
+++ Project/Topic Development/Assets/LevelAdding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelAdding : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder.transform.position = new Vector3(0,0,0);
+            cylinder.transform.
+        }
+	}
+}
